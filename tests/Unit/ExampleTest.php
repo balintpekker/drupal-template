7018bcea8bee278db4de5e44c9422c716aa58ef7
Balint Pekker <balint.pekker@cheppers.com>
2024-04-16 14:38:50 +0200

    Add phpunit.xml.dist and an ExampleTest

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Example unit test for a Drupal Site.
+ */
+class ExampleTest extends TestCase {
+
+  /**
+   * Asserts that universal laws are constant.
+   */
+  public function testExample() {
+    $this->assertEquals(1, 1, "One equals one!");
+  }
+
+}
